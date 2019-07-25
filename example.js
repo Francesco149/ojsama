9f9d4ab5f07ba9368d83883da47914d70ece7543
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-07-26 02:24:31 +0530

    Changes to example and Formatting.

--- example.js
+++ example.js
@@ -1,60 +1,54 @@
-var readline = require("readline");
-var osu = require("./ojsama");
+const readline = require('readline');
+const osu = require('./src');
 
-var mods = osu.modbits.none;
-var acc_percent;
-var combo;
-var nmiss;
+let mods = osu.modbits.none;
+let acc_percent;
+let combo;
+let nmiss;
 
 // get mods, acc, combo, misses from command line arguments
 // format: +HDDT 95% 300x 1m
-var argv = process.argv;
+const argv = process.argv;
 
-for (var i = 2; i < argv.length; ++i)
-{
-    if (argv[i].startsWith("+")) {
-        mods = osu.modbits.from_string(argv[i].slice(1) || "");
-    }
-
-    else if (argv[i].endsWith("%")) {
-        acc_percent = parseFloat(argv[i]);
-    }
-
-    else if (argv[i].endsWith("x")) {
-        combo = parseInt(argv[i]);
-    }
-
-    else if (argv[i].endsWith("m")) {
-        nmiss = parseInt(argv[i]);
-    }
+for (let i = 2; i < argv.length; ++i) {
+  if (argv[i].startsWith('+')) {
+    mods = osu.modbits.from_string(argv[i].slice(1) || '');
+  } else if (argv[i].endsWith('%')) {
+    acc_percent = parseFloat(argv[i]);
+  } else if (argv[i].endsWith('x')) {
+    combo = parseInt(argv[i]);
+  } else if (argv[i].endsWith('m')) {
+    nmiss = parseInt(argv[i]);
+  }
 }
 
-var parser = new osu.parser();
-readline.createInterface({input: process.stdin, terminal: false})
-.on("line", parser.feed_line.bind(parser))
-.on("close", function() {
-    var map = parser.map;
-    console.log(map.toString());
+const parser = new osu.parser();
+readline
+    .createInterface({input: process.stdin, terminal: false})
+    .on('line', parser.feed_line.bind(parser))
+    .on('close', function() {
+      const map = parser.map;
+      console.log(map.toString());
 
-    if (mods) {
-        console.log("+" + osu.modbits.string(mods));
-    }
+      if (mods) {
+        console.log('+' + osu.modbits.string(mods));
+      }
 
-    var stars = new osu.diff().calc({map: map, mods: mods});
-    console.log(stars.toString());
+      const stars = new osu.diff().calc({map: map, mods: mods});
+      console.log(stars.toString());
 
-    var pp = osu.ppv2({
+      const pp = osu.ppv2({
         stars: stars,
         combo: combo,
         nmiss: nmiss,
         acc_percent: acc_percent,
+      });
+
+      const max_combo = map.max_combo();
+      combo = combo || max_combo;
+
+      console.log(pp.computed_accuracy.toString());
+      console.log(combo + '/' + max_combo + 'x');
+
+      console.log(pp.toString());
     });
-
-    var max_combo = map.max_combo();
-    combo = combo || max_combo;
-
-    console.log(pp.computed_accuracy.toString());
-    console.log(combo + "/" + max_combo + "x");
-
-    console.log(pp.toString());
-});
