47f0dcc615bafbbe0c9514ce68c32d7e5e5e8abf
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-07-26 02:22:37 +0530

    Bring Index to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-/*
+const {timing} = require('./timing');
+const {objtypes, circle, slider, hitobject} = require('./hitobjects');
+const {modes, beatmap} = require('./beatmap');
+const {parser} = require('./parser');
+const {std_accuracy, std_ppv2, ppv2} = require('./ppclac');
+
+const {
+  modbits,
+  std_diff,
+  std_beatmap_stats,
+  std_diff_hitobject,
+  diff,
+} = require('./difficultycalc');
 // TODO : This will be at the last.
-module.exports = osu = {
+const osu = {
   timing: timing,
   objtypes: objtypes,
   circle: circle,
@@ -20,4 +32,6 @@ module.exports = osu = {
   VERSION_MAJOR: 1,
   VERSION_MINOR: 2,
   VERSION_PATCH: 1,
-};*/
+};
+
+module.exports = osu;
