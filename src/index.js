c483914405400b2f3545b6f9f1c1e1e393ed6edd
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-07-26 06:22:35 +0530

    Update index.js to use changed reflect difficulty.js.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
-const {timing} = require('./timing');
-const {objtypes, circle, slider, hitobject} = require('./hitobjects');
-const {modes, beatmap} = require('./beatmap');
-const {parser} = require('./parser');
-const {std_accuracy, std_ppv2, ppv2} = require('./ppclac');
-
+const {Timing} = require('./timing');
+const {Objtypes, Circle, Slider, Hitobject} = require('./hitobjects');
+const {modes, Beatmap} = require('./beatmap');
+const {Parser} = require('./parser');
+const {StdAccuracy, StdPPv2, ppv2} = require('./ppclac');
 const {
   modbits,
-  std_diff,
-  std_beatmap_stats,
-  std_diff_hitobject,
-  diff,
+  StdDiff,
+  StdBeatmapStats,
+  StdDiffHitobject,
+  Diff,
 } = require('./difficultycalc');
 // TODO : This will be at the last.
 const osu = {
-  timing: timing,
-  objtypes: objtypes,
-  circle: circle,
-  slider: slider,
-  hitobject: hitobject,
+  Timing: Timing,
+  Objtypes: Objtypes,
+  Circle: Circle,
+  Slider: Slider,
+  Hitobject: Hitobject,
   modes: modes,
-  beatmap: beatmap,
-  parser: parser,
+  Beatmap: Beatmap,
+  Parser: Parser,
   modbits: modbits,
-  std_beatmap_stats: std_beatmap_stats,
-  std_diff_hitobject: std_diff_hitobject,
-  std_diff: std_diff,
-  diff: diff,
-  std_accuracy: std_accuracy,
-  std_ppv2: std_ppv2,
+  StdBeatmapStats: StdBeatmapStats,
+  StdDiffHitobject: StdDiffHitobject,
+  StdDiff: StdDiff,
+  Diff: Diff,
+  std_accuracy: StdAccuracy,
+  std_ppv2: StdPPv2,
   ppv2: ppv2,
   VERSION_MAJOR: 1,
   VERSION_MINOR: 2,
   VERSION_PATCH: 1,
 };
 
-module.exports = osu;
+module.exports = {
+  osu: osu,
+};
